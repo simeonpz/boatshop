782adb150eff1d8b619bd1ebb279d8f093224bdd
simeon <simeon99pz@abv.bg>
2024-01-27 13:23:30 +0200

    Second update - major changes

--- indexScript.js
+++ indexScript.js
@@ -77,6 +77,19 @@ const closeNavBar = (e) => {
 burgerMenu.addEventListener('click', (e) => closeNavBar(e))
 
 
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
 
 
 
